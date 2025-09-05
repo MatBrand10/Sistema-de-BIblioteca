c711d42f0a1ccd8500980a2f736957f341cf586e
Matheus.R.Brandao <153210805+MatBrand10@users.noreply.github.com>
2025-09-05 15:35:39 -0300

    Update main.py

--- biblioteca/main.py
+++ biblioteca/main.py
@@ -3,22 +3,18 @@ from professor import Professor
 from livro import Livro
 from servicos import emprestar_livro, devolver_livro, relatorio_emprestimos
 
-# Cadastro dos usuários
-aluno1 = Aluno("João", "2021001")
-prof1 = Professor("Maria", "P1001")
+aluno1 = Aluno("Matheus", "2021001")
+prof1 = Professor("Dua", "P1001")
 
-# Cadastro dos livros
 livro1 = Livro("POO para Iniciantes", "Autor X")
 livro2 = Livro("Python Essencial", "Autor Y")
 
 usuarios = [aluno1, prof1]
 livros = [livro1, livro2]
 
-# Emprestar livros
 emprestar_livro(aluno1, livro1)
 emprestar_livro(prof1, livro2)
 
-# Tentar ultrapassar o limite do aluno
 for i in range(3):
     emprestar_livro(aluno1, Livro(f"Livro Extra {i}", "Autor Teste"))
 
